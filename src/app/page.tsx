9e069966dd0ca91c1b45607ecde24a1628d50259
haochen <sumtsui@gmail.com>
2025-02-18 12:53:08 +0800

    add project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,6 +55,30 @@ export default function Home() {
           <p className="text-lg text-white mb-3">{experience.title}</p>
         </div>
       ))}
+      <h2 data-text="Projects" className={`mb-6 mt-[2000px] ${styles.glitch}`}>
+        Projects
+      </h2>
+      <div className="bg-white text-black p-4">
+        <h3>
+          <a
+            href="https://declutterspace.net"
+            className="decoration-solid font-bold"
+          >
+            declutterspace.net
+          </a>
+        </h3>
+        <p>
+          I’ve always yearned for a minimalist lifestyle. I don’t want my home
+          to be a storage space; instead, I want all my belongings to serve a
+          purpose. I guess I have a bit of a compulsive tendency when it comes
+          to my stuff. So, I created this app to help with decluttering and
+          organizing items. It takes a deadline-based approach to getting rid of
+          things. I use LLM for photo recognition, allowing users to quickly
+          upload items in bulk for easier management. Also, it&apos;s a good
+          exercise to try out the new and shiny Next.js 15 features.
+        </p>
+      </div>
+
       <ThreeScene />
       <div className="absolute bottom-0 left-0 w-full text-center text-lg text-white p-8">
         Stay in touch: sumtsui@gmail.com
