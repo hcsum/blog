22694be1018c60bd9d3871dcdaa3b07b02c9ed4a
haochen <sumtsui@gmail.com>
2025-02-19 21:52:54 +0800

    fix styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,58 +33,64 @@ export default function Home() {
         </a>
         .
       </p>
-      <h2
-        data-text="Experience"
-        className={`mb-6 mt-[2000px] ${styles.glitch}`}
-      >
-        Experience
-      </h2>
-      {experiences.map((experience) => (
-        <div
-          key={experience.startDate}
-          className="mb-8 p-6 rounded-lg bg-black/5 hover:bg-black/10 transition-colors"
+      <section className="mt-[800px] mb-64">
+        <h2
+          data-text="Experience"
+          className={`mb-6 mt-[2000px] ${styles.glitch}`}
         >
-          <div className="flex justify-between items-center mb-2">
-            <h3 className="text-xl font-semibold text-gray-300">
-              {experience.company}
-            </h3>
-            <span className="text-sm text-gray-300">
-              {experience.startDate} - {experience.endDate || "Present"}
-            </span>
-          </div>
-          <p className="text-lg text-white mb-3">{experience.title}</p>
-        </div>
-      ))}
-      <h2 data-text="Projects" className={`mb-6 mt-[2000px] ${styles.glitch}`}>
-        Projects
-      </h2>
-      <div className="bg-white text-black p-4">
-        <h3 className="mb-2">
-          <a
-            href="https://declutterspace.net"
-            className="decoration-solid font-bold"
-            target="_blank"
+          Experience
+        </h2>
+        {experiences.map((experience) => (
+          <div
+            key={experience.startDate}
+            className="mb-8 p-6 rounded-lg bg-black/5 hover:bg-black/10 transition-colors"
           >
-            declutterspace.net
-          </a>
-        </h3>
-        <p>
-          I’ve always yearned for a minimalist lifestyle. I don’t want my home
-          to be a storage space; instead, I want all my belongings to serve a
-          purpose. I guess I have a bit of a compulsive tendency when it comes
-          to my stuff. So, I created this app to help people like me with
-          decluttering and organizing items. It takes a deadline-based approach
-          to getting rid of things. I use LLM for photo recognition, allowing
-          users to quickly upload items in bulk for easier management. Also,
-          it&apos;s a good exercise to try out the new and shiny Next.js 15
-          features.
-        </p>
-      </div>
-
-      <ThreeScene />
+            <div className="flex justify-between items-center mb-2">
+              <h3 className="text-xl font-semibold text-gray-300">
+                {experience.company}
+              </h3>
+              <span className="text-sm text-gray-300">
+                {experience.startDate} - {experience.endDate || "Present"}
+              </span>
+            </div>
+            <p className="text-lg text-white mb-3">{experience.title}</p>
+          </div>
+        ))}
+      </section>
+      <section className="mt-[1000px] mb-64">
+        <h2
+          data-text="Projects"
+          className={`mb-6 mt-[1000px] ${styles.glitch}`}
+        >
+          Projects
+        </h2>
+        <div className="bg-white text-black p-4">
+          <h3 className="mb-2">
+            <a
+              href="https://declutterspace.net"
+              className="decoration-solid font-bold"
+              target="_blank"
+            >
+              declutterspace.net
+            </a>
+          </h3>
+          <p>
+            I’ve always yearned for a minimalist lifestyle. I don’t want my home
+            to be a storage space; instead, I want all my belongings to serve a
+            purpose. I guess I have a bit of a compulsive tendency when it comes
+            to my stuff. So, I created this app to help people like me with
+            decluttering and organizing items. It takes a deadline-based
+            approach to getting rid of things. I use LLM for photo recognition,
+            allowing users to quickly upload items in bulk for easier
+            management. Also, it&apos;s a good exercise to try out the new and
+            shiny Next.js 15 features.
+          </p>
+        </div>
+      </section>
       <div className="absolute bottom-0 left-0 w-full text-center text-lg text-white p-8">
         Stay in touch: sumtsui@gmail.com
       </div>
+      <ThreeScene />
     </div>
   );
 }
